202a31ea13445072aee56ea62244b3c4f9ecfcdd
Alexander Westberg <alexander.bladh@gmail.com>
2026-08-11 11:48:59 +0200

    Fix newAztecCode to use provided color scheme instead of hardcoded value

--- aztec/azteccode.go
+++ aztec/azteccode.go
@@ -17,7 +17,7 @@ type aztecCode struct {
 }
 
 func newAztecCode(size int, color barcode.ColorScheme) *aztecCode {
-	return &aztecCode{utils.NewBitList(size * size), size, nil, barcode.ColorScheme16}
+	return &aztecCode{utils.NewBitList(size * size), size, nil, color}
 }
 
 func (c *aztecCode) Content() string {
